33e10bbee9d3d27727272f43dd124c827a516971
aquillae777 <aquillae777@gmail.com>
2023-03-08 13:27:42 -0300

    coletando html da pasta livrosLista

--- livros-react--templete/src/App.js
+++ livros-react--templete/src/App.js
@@ -1,13 +1,7 @@
 
 import './index.css';
 import React from 'react';
-import './'
-
-
-
-
-
-
+import Tabela  from "./LivrosLista"
 
 
 
@@ -16,67 +10,11 @@ import './'
 
 function App() {
   return (
-    <div className="App">
-        <header>
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <div class="navbar-collapse">
-        
-          <ul class="navbar-nav">
-              <li class="nav-item">
-                  <a class="nav-link active" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="receitas.html">catálogo</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="cadastro.html">Novo</a>
-              </li>
-          </ul>
-      </div>
-  </nav>
-  </header>
-  <h1 scope="row" >Titulo</h1>
-  <br/>
-  <div className='container'>
-  <div className='tabela'> 
-    </div>
-    <table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">Titulo</th>
-      <th scope="col">resumo</th>
-      <th scope="col">editora</th>
-      <th scope="col">Autores</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"> Use o cabeçalho Java  <button className='button'>excluir</button></th>
-      
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row"> Titulo <button className='button'>excluir</button></th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row"> Titulo <button className='button'>excluir</button></th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-  </div>
-
+<>
+ <Tabela/>
+</>
     
 
-   
-    </div>
   );
 }
 
