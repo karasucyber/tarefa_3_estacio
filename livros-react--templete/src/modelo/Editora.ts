c7e1266ba0ad9e4c5c2b4c35a8799ce2b9c1b32d
aquillae777 <aquillae777@gmail.com>
2023-03-07 23:12:59 -0300

    criando a class

--- livros-react--templete/src/modelo/Editora.ts
+++ livros-react--templete/src/modelo/Editora.ts
@@ -1,5 +1,16 @@
-
-
-class editora(){
+export class Editora{
     
+    public codEditora: number;
+    public nome: string;
+
+ /////////////////////////////////
+    
+ constructor(codEditora: number, nome: string) {
+
+   this.codEditora = codEditora ;
+    
+   this.nome = nome;
 }
+}
+
+
