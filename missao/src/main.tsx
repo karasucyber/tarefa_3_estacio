589ebe134ef3fca4c4e461bae42763ea81dcf9c6
aquillae777 <aquillae777@gmail.com>
2023-03-13 10:14:14 -0300

    criando rotas

--- missao/src/main.tsx
+++ missao/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './pag/App';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pag/Home"
+import Cadastro from "./pag/Cadastro";
+import "bootstrap/dist/css/bootstrap.min.css" ; 
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element={<App/>}/>
+      <Route path="/Home" element={<Home/>}/>
+      <Route path="/cadastro" element={<Cadastro/>}/>
+     </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
