7179584d55db87d4da1f382270c46da479f01835
aquillae777 <aquillae777@gmail.com>
2023-03-13 10:16:28 -0300

    Cadastro

---
+++ missao/src/pag/Cadastro.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Barra from "../componente/barra";
+import Tabela from "../componente/tabela";
+import fomulário from "../componente/formulario";
+import Coletor from "../componente/formulario";
+
+
+const cadastro = () => {
+    return(
+    <>
+    <Barra/>
+    <Coletor/>
+    </>
+    
+)}
+
+export default cadastro;
