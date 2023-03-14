5568c1e1d8f6d1342e518f3705c875c5ee4b2ce9
aquillae777 <aquillae777@gmail.com>
2023-03-14 19:08:56 -0300

    componente

--- missao/src/componente/tabela.tsx
+++ missao/src/componente/tabela.tsx
@@ -1,25 +1,5 @@
-import { ControleEditor, editoras } from "../controle/ControleEditoras";
-import {LivrosControles} from "../controle/controleLivros"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import Linhas from "./linha"
+import React from "react";
 
 
 function Tabela(){
@@ -29,43 +9,19 @@ function Tabela(){
 <table className="table">
   <thead>
     <tr>
-      <th scope="col-">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-      <button type="button" className="btn btn-dark">Excluir</button>
-
+      <th scope="col-">titulo</th>
+      <th scope="col">resumo</th>
+      <th scope="col">Editora</th>
+      <th scope="col">Autores</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <button type="button" className="btn btn-dark">Excluir</button>
+    
 
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <button type="button" className="btn btn-dark">Excluir</button>
-
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td >Larry the Bird</td>
-      <td>@twitter</td>
-      <td> olá </td>
-      <button type="button" className="btn btn-dark">Excluir</button>
-
-    </tr>
   </tbody>
 </table>
 </div>
+      
         </>
     );
 }
